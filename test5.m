13b74563c57eb63635953e29ffefe2208770358e
ZhangCheng <hustquick@gmail.com>
2016-07-13 15:41:53 +0800

    "Add a 3D plot of the Stirling engine."

--- test5.m
+++ test5.m
@@ -1,10 +1,33 @@
+% test of 3D plot
 clear;
-st1 = Stream;
-st1.fluid = char(Const.Fluid(1));
-st1.q_m.v = 1;
-st1.T.v = 1073.15;
-st1.p.v = 5e5;
+close all;
+T_H = 300 : 10 : 1000;
+T_L = 300 : 10 : 1000;
+DeltaT = 0 : 10 : 700;
+[x1, y1] = meshgrid(T_H, T_L);
+[x2, y2] = meshgrid(T_H, DeltaT);
+eta1 = eta(x1, y1);
+eta2 = eta(x2, x2 - y2);
+for i = 1 : numel(eta1(:,1))
+    for j = 1 : numel(eta1(1,:))
+        if (x1(i,j) < y1(i,j))
+            eta1(i,j) = NaN;
+        end
+        if (x2(i,j) < y2(i,j))
+            eta2(i,j) = NaN;
+        end
+    end
+end
+subplot(1,2,1);
+h1 = mesh(x1, y1, eta1);
+% axis([xlim ylim 0 0.4]);
+xlabel('T_H');
+ylabel('T_L');
+zlabel('\eta');
 
-sea = SEC(3,'Series');
-sea.st1_i = st1;
-sea.calculate;
+% figure('NextPlot', 'add');
+subplot(1,2,2);
+h2 = mesh(x2, y2, eta2);
+xlabel('T_H');
+ylabel('\Delta{}T');
+zlabel('\eta');
